66470675b3af7c7ae1896fbfee2505e09a52a85b
Guilherme Octávio <guilhermeoctavio01@gmail.com>
2023-12-24 18:33:30 -0300

    Projeto criado

---
+++ dartlearning/bin/dartlearning.dart
@@ -0,0 +1,15 @@
+// Null off
+// Variaveis
+// caso você não sabe qual o valor essa variavel ira receber apenas declare ela,
+//mas o Null tem que esta on.
+// String name;
+
+String name = 'Guilherme';
+
+// Caso voc
+
+
+
+void main() {
+  // todo dart tem que ter um void main
+}
